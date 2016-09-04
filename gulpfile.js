18f0e6a32e85036fb49e7cb95a0679f7f68b7e6f
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-09-04 20:24:36 +0400

    Major: extend to support writings
    - not just projects

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,21 @@ const bower = require('gulp-bower')
 const stylus = require('gulp-stylus')
 const connect = require('gulp-connect')
 const ghPages = require('gulp-gh-pages')
+const rename = require('gulp-rename')
 const marked = require('marked')
 const moment = require('moment')
 const _ = require('lodash')
-
+const fs = require('fs')
+var del = require('del')
 
 const build_dir = './build/'
+marked.setOptions({
+  breaks: true
+});
 
 gulp.task('bower', () => {
   return bower()
     .pipe(gulp.dest(build_dir + 'bower_components/'))
-})
-
-gulp.task('markdown', () => {
-  return gulp.src('./projects/**/*.md')
-    .pipe(gutil.buffer())
-    .pipe(markdownToJSON(marked, 'posts.json'))
-    .pipe(gulp.dest(build_dir))
 })
 
 gulp.task('copy_public', () => {
@@ -32,18 +30,59 @@ gulp.task('copy_public', () => {
     .pipe(gulp.dest(build_dir))
 })
 
-gulp.task('pug', ['markdown'], () => {
+gulp.task('markdown-projects', (cb) => {
+  return gulp.src('./projects/**/*.md')
+    .pipe(gutil.buffer())
+    .pipe(markdownToJSON(marked, 'projects.json'))
+    .pipe(gulp.dest(build_dir + 'data/'))
+})
+
+gulp.task('projects', ['markdown-projects'], () => {
   return gulp.src('./views/index.pug')
     .pipe(data(() => {
-        const json_file = require(build_dir + 'posts.json')
-        return { 
-          projects: _.reverse(_.sortBy(_.values(json_file), 'importance')),
-          moment: moment
-        }
-      }))
+      const json_file = require(build_dir + 'data/projects.json')
+      return {
+        projects: _.reverse(_.sortBy(_.values(json_file), 'importance')),
+        moment: moment
+      }
+    }))
     .pipe(pug())
+    .pipe(gulp.dest(build_dir + 'projects/'))
+})
+
+gulp.task('markdown-writing', ['markdown-projects'], (cb) => {
+  return gulp.src('./writings/**/*.md')
+    .pipe(gutil.buffer())
+    .pipe(markdownToJSON(marked, 'writings.json'))
+    .pipe(gulp.dest(build_dir + 'data/'))
+})
+
+gulp.task('writing', ['markdown-writing'], () => {
+  return gulp.src('./views/index.pug')
+    .pipe(data(() => {
+      const json_file = require(build_dir + 'data/writings.json')
+      return {
+        writings: _.sortBy(_.values(json_file), function (elem) {
+          return -Date.parse(elem.start)
+        }),
+        moment: moment
+      }
+    }))
+    .pipe(pug())
+    .pipe(gulp.dest(build_dir + 'writings/'))
+})
+
+gulp.task('clean:data', ['writing', 'projects'], function () {
+  return del(build_dir + 'data/')
+})
+
+gulp.task('make_index', ['projects'], () => {
+  return gulp
+    .src(build_dir + 'projects/*')
     .pipe(gulp.dest(build_dir))
 })
+
+gulp
 
 gulp.task('connect', () => {
   connect.server({
@@ -62,4 +101,5 @@ gulp.task('deploy', () => {
     .pipe(ghPages({branch: 'master'}))
 })
 
-gulp.task('default', ['bower', 'markdown', 'pug', 'css', 'copy_public'])
+gulp.task('content', ['writing', 'projects'])
+gulp.task('default', ['bower', 'content', 'css', 'copy_public', 'make_index', 'clean:data'])
